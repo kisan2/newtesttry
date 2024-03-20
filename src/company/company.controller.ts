d64c9e9fdaefcdaf160db53233467c013f5a7cc0
kisan2 <53286712+kisan2@users.noreply.github.com>
2024-03-20 21:55:42 +0545

    Update company.controller.ts

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -5,7 +5,7 @@ import { Roles } from 'src/decorator/Role.decorator';
 import { roles } from 'src/util/roles';
 import { CanAccess } from 'src/Guard/CanAccess.guard';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
-import { CompanyInfoDto } from './dto/createcompanyInfo.dto';
+// import { CompanyInfoDto } from './dto/createcompanyInfo.dto';
 import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from 'src/config/multer.config';
 import { createWalletDto } from './dto/createWallet.dto';
@@ -15,24 +15,24 @@ import { createWalletDto } from './dto/createWallet.dto';
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
-  @Post('postCompanyInfo')
-  @Roles(roles.admin)
-  @UseGuards(CanAccess)
-  @ApiBearerAuth("jwt")
-  @ApiBody({type:CompanyInfoDto})
-  @ApiConsumes('multipart/form-data')
-  @UseInterceptors(FileFieldsInterceptor([
-    {name:'depositQr',maxCount:1},
-    {name:"banner",maxCount:1},
-    {name:"logo",maxCount:1}
-  ],multerOptions))
-  async addBasicInfo(@UploadedFiles() files:{ depositQr?: Express.Multer.File[], banner?: Express.Multer.File[],logo?: Express.Multer.File[] },@Body(new ValidationPipe({whitelist: true})) companyInfo:CompanyInfoDto){
-    try{
-      return await this.companyService.addBasicInfo(files,companyInfo);
-    }catch(err){
-      throw new HttpException(err,HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-  }
+  // @Post('postCompanyInfo')
+  // @Roles(roles.admin)
+  // @UseGuards(CanAccess)
+  // @ApiBearerAuth("jwt")
+  // @ApiBody({type:CompanyInfoDto})
+  // @ApiConsumes('multipart/form-data')
+  // @UseInterceptors(FileFieldsInterceptor([
+  //   {name:'depositQr',maxCount:1},
+  //   {name:"banner",maxCount:1},
+  //   {name:"logo",maxCount:1}
+  // ],multerOptions))
+  // async addBasicInfo(@UploadedFiles() files:{ depositQr?: Express.Multer.File[], banner?: Express.Multer.File[],logo?: Express.Multer.File[] },@Body(new ValidationPipe({whitelist: true})) companyInfo:CompanyInfoDto){
+  //   try{
+  //     return await this.companyService.addBasicInfo(files,companyInfo);
+  //   }catch(err){
+  //     throw new HttpException(err,HttpStatus.INTERNAL_SERVER_ERROR)
+  //   }
+  // }
 
   @Post('postWalletInfo')
   @Roles(roles.admin)
