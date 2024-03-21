c450c246e17afe79350de0076ec62776e9fdf2ac
kisan2 <kisanmahat2@gmail.com>
2024-03-21 13:45:19 +0545

    here 4

--- src/config/multer_g.config.ts
+++ src/config/multer_g.config.ts
@@ -3,7 +3,7 @@ import { diskStorage } from "multer"
 import { extname } from "path"
 export const multerOptions =  {
     storage: diskStorage({
-      destination: '../uploads'
+      destination: 'dist'
       , filename: (req, file, cb) => {
         // Generating a 32 random chars long string
         const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
